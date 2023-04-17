f303081d142fd4e1a95e5fdf00bb345217453fcd
Andrey <dobrenko2000@mail.ru>
2023-04-18 00:23:25 +0300

    last 17.04

--- static/scripts/main_page_markup.js
+++ static/scripts/main_page_markup.js
@@ -1,5 +1,26 @@
 "use strict";
-
+function chengeFormsPath(){
+    let BodyW = document.getElementById('bodys');
+    // Можно будет попробовать чере цикл, но я не знаю, как это сделать(((
+    /*let forms = document.getElementsByClassName('widget_form');
+    for (const OneForm of forms) {
+        let NewSrc = OneForm.src.replace('/static/img/forms/', '');
+        console.log(NewSrc);
+    }*/
+    if (BodyW.offsetWidth < 1279){
+        document.getElementById('lessons_bg').src ='/static/img/forms/mobile/lessons_form.png';
+        document.getElementById('projects_bg').src ='/static/img/forms/mobile/projects_form.png';
+        document.getElementById('msg_bg').src ='/static/img/forms/mobile/msg_form.png';
+        document.getElementById('ntf_bg').src ='/static/img/forms/mobile/ntf_form.png';
+        document.getElementById('records_bg').src ='/static/img/forms/mobile/records_form.png';
+    }else{
+        document.getElementById('lessons_bg').src ='/static/img/forms/lessons_form.png';
+        document.getElementById('projects_bg').src ='/static/img/forms/projects_form.png';
+        document.getElementById('msg_bg').src ='/static/img/forms/msg_form.png';
+        document.getElementById('ntf_bg').src ='/static/img/forms/ntf_form.png';
+        document.getElementById('records_bg').src ='/static/img/forms/records_form.png';
+    }
+}
 
 function setFormSize() {
     const divsWithImages = document.querySelectorAll('#main_content > div');
@@ -30,6 +51,7 @@ window.onload = function () {
     setRelativeFont('#areas_progress > div', 'progress', 2);
     setRelativeFont('#point_progress > div', 'point_progress', 1.5);
     setRelativeFont('#progress > span', 'progress', 1.4);
+    chengeFormsPath();
     setFormSize();
 }
 
@@ -37,6 +59,7 @@ window.onresize = function(){
     setRelativeFont('#areas_progress > div', 'progress', 2);
     setRelativeFont('#point_progress > div', 'point_progress', 1.5);
     setRelativeFont('#progress > span', 'progress', 1.4);
+    chengeFormsPath();
     setFormSize();
 }
 
